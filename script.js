0120f600a4c1fa32b6009cff77a003c0549a1d9e
verakissyou <veronicaciuca17@gmail.com>
2025-01-01 21:01:48 +0200

    Modify the mobile touch events

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-const audio =  document.querySelector
-('audio');
+const audio = document.querySelector('audio');
 const slider = document.querySelector('.items');
 let isDown = false;
 let startX;
@@ -11,7 +10,7 @@ slider.addEventListener('mousedown', (e) => {
   startX = e.pageX - slider.offsetLeft;
   scrollLeft = slider.scrollLeft;
   audio.play();
-}); 
+});
 
 slider.addEventListener('mouseleave', (e) => {
   isDown = false;
@@ -26,39 +25,41 @@ slider.addEventListener('mouseup', (e) => {
 });
 
 slider.addEventListener('mousemove', (e) => {
-if(!isDown) return;
-e.preventDefault();
-const x = e.pageX - slider.offsetLeft;
-const walk = (x - startX) * 3;
-slider.scrollLeft = scrollLeft - walk;
-audio.play();
-});
-
-slider.addEventListener('touchstart', (e) => {
-  isDown = true;
-  slider.classList.add('active');
-  startX = e.pageX - slider.offsetLeft;
-  scrollLeft = slider.scrollLeft;
-  audio.play();
-}); 
-
-slider.addEventListener('touchend', (e) => {
-  isDown = false;
-  slider.classList.remove('active');
-  audio.pause();
-});
-
-slider.addEventListener('touchcancel', (e) => {
-  isDown = false;
-  slider.classList.remove('active');
-  audio.pause();
-});
-
-slider.addEventListener('touchmove', (e) => {
-  if(!isDown) return;
+  if (!isDown) return;
   e.preventDefault();
   const x = e.pageX - slider.offsetLeft;
   const walk = (x - startX) * 3;
   slider.scrollLeft = scrollLeft - walk;
   audio.play();
-  });
+});
+
+slider.addEventListener('touchstart', (e) => {
+  isDown = true;
+  slider.classList.add('active');
+  startX = e.touches[0].pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+  audio.play();
+  if(audio.paused) {
+    audio.play();
+  }
+});
+
+slider.addEventListener('touchend', (e) => {
+  isDown = false;
+  slider.classList.remove('active');
+  if(audio.played) {
+    audio.pause();
+  }
+});
+
+slider.addEventListener('touchmove', (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.touches[0].pageX - slider.offsetLeft;
+  const walk = (x - startX) * 3;
+  slider.scrollLeft = scrollLeft - walk;
+  audio.play();
+  if(audio.paused) {
+    audio.play();
+  }
+});
