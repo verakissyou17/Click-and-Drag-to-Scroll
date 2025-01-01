efc9d955e048a304daa48dded3ee9883f8aef9ab
verakissyou <veronicaciuca17@gmail.com>
2025-01-01 20:24:21 +0200

    Add events for mobile

--- script.js
+++ script.js
@@ -33,3 +33,32 @@ const walk = (x - startX) * 3;
 slider.scrollLeft = scrollLeft - walk;
 audio.play();
 });
+
+slider.addEventListener('touchstart', (e) => {
+  isDown = true;
+  slider.classList.add('active');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+  audio.play();
+}); 
+
+slider.addEventListener('touchend', (e) => {
+  isDown = false;
+  slider.classList.remove('active');
+  audio.pause();
+});
+
+slider.addEventListener('touchcancel', (e) => {
+  isDown = false;
+  slider.classList.remove('active');
+  audio.pause();
+});
+
+slider.addEventListener('touchmove', (e) => {
+  if(!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 3;
+  slider.scrollLeft = scrollLeft - walk;
+  audio.play();
+  });
