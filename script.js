93f8765046c5e5a2db076767341f45f409f497af
verakissyou <veronicaciuca17@gmail.com>
2025-06-22 15:39:05 +0300

    Update the animation state and add audio element back

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 const slider = document.querySelector(".items");
+const snowflakes = document.querySelectorAll(".snowflake");
+const audio = document.querySelector("audio");
 let isDown = false;
 let startX;
 let scrollLeft;
+
+audio.pause();
 
 slider.addEventListener("mousedown", (e) => {
   isDown = true;
   slider.classList.add("active");
   startX = e.pageX - slider.offsetLeft;
   scrollLeft = slider.scrollLeft;
+  snowflakes.forEach((snowflake) => {
+    snowflake.classList.add("active");
+  });
+  audio.play(); 
 });
 
 slider.addEventListener("mouseup", (e) => {
+  isDown = false;
+  slider.classList.remove("active");
+});
+
+slider.addEventListener("mouseleave", (e) => {
   isDown = false;
   slider.classList.remove("active");
 });
@@ -26,11 +39,20 @@ slider.addEventListener("mousemove", (e) => {
 slider.addEventListener("touchstart", (e) => {
   isDown = true;
   slider.classList.add("active");
+  snowflakes.forEach((snowflake) => {
+    snowflake.classList.add("active");
+  });
   startX = e.touches[0].pageX - slider.offsetLeft;
   scrollLeft = slider.scrollLeft;
+  audio.play();
 });
 
 slider.addEventListener("touchend", (e) => {
+  isDown = false;
+  slider.classList.remove("active");
+});
+
+slider.addEventListener("touchcancel", (e) => {
   isDown = false;
   slider.classList.remove("active");
 });
