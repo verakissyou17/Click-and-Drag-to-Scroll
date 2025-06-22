fc310036c09eb927b57dd75e63f9d2387cf455b9
verakissyou <veronicaciuca17@gmail.com>
2025-06-22 14:20:01 +0300

    Delete audio element

--- script.js
+++ script.js
@@ -1,60 +1,44 @@
-const audio = document.querySelector('audio');
-const slider = document.querySelector('.items');
+const slider = document.querySelector(".items");
 let isDown = false;
 let startX;
 let scrollLeft;
 
-slider.addEventListener('mousedown', (e) => {
+slider.addEventListener("mousedown", (e) => {
   isDown = true;
-  slider.classList.add('active');
+  slider.classList.add("active");
   startX = e.pageX - slider.offsetLeft;
   scrollLeft = slider.scrollLeft;
-  audio.play();
 });
 
-
-slider.addEventListener('mouseup', (e) => {
+slider.addEventListener("mouseup", (e) => {
   isDown = false;
-  slider.classList.remove('active');
-  audio.pause();
+  slider.classList.remove("active");
 });
 
-slider.addEventListener('mousemove', (e) => {
+slider.addEventListener("mousemove", (e) => {
   if (!isDown) return;
   e.preventDefault();
   const x = e.pageX - slider.offsetLeft;
   const walk = (x - startX) * 3;
   slider.scrollLeft = scrollLeft - walk;
-  audio.play();
 });
 
-slider.addEventListener('touchstart', (e) => {
+slider.addEventListener("touchstart", (e) => {
   isDown = true;
-  slider.classList.add('active');
+  slider.classList.add("active");
   startX = e.touches[0].pageX - slider.offsetLeft;
   scrollLeft = slider.scrollLeft;
-  audio.play();
-  if(audio.paused) {
-    audio.play();
-  }
 });
 
-slider.addEventListener('touchend', (e) => {
+slider.addEventListener("touchend", (e) => {
   isDown = false;
-  slider.classList.remove('active');
-  if(audio.played) {
-    audio.pause();
-  }
+  slider.classList.remove("active");
 });
 
-slider.addEventListener('touchmove', (e) => {
+slider.addEventListener("touchmove", (e) => {
   if (!isDown) return;
   e.preventDefault();
   const x = e.touches[0].pageX - slider.offsetLeft;
   const walk = (x - startX) * 3;
   slider.scrollLeft = scrollLeft - walk;
-  audio.play();
-  if(audio.paused) {
-    audio.play();
-  }
 });
